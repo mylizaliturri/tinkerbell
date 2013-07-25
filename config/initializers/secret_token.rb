7ada61e25433f35157f6a0ae7a26133321c2053c
Miguel Angel Ylizaliturri Salcedo <miguel.ylizaliturri@gmail.com>
2013-07-25 07:22:30 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinkerbell::Application.config.secret_token = 'd4fd492295da0e692293c760842b50ebdf8c7eefc040cfc7de34d423de662b8b596fa1118314ea14cf7ba194c341050e4f19b63057d07e6b3edec6022df41f33'
